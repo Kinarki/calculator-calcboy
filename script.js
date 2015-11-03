e783a492b5fadc07486695610814b1f37d82a286
Kinarki <nickfriend81@gmail.com>
2015-11-03 14:02:01 -0800

    Worked on math functionality without eval.

--- script.js
+++ script.js
@@ -2,20 +2,25 @@
 var num_array = [''];
 var i = 0;
 var operator = null;
-var result;
+var num1 = null;
+var num2 = null;
+var op = null;
+var result = null;
 
-function input_digit(number) {
+
+function inputDigit(number) {
     num_array[i] += number;
-    refresh_display();
+    refreshDisplay();
 }
 
-function refresh_display() {
+function refreshDisplay() {
     $('#output').text(num_array.join(' '));
 }
-function increment_array(oper) {
+
+function incrementArray(oper) {
     i++;
     num_array.push(oper);
-    refresh_display();
+    refreshDisplay();
     i++;
     num_array.push('');
 }
@@ -24,24 +29,24 @@ $(document).ready(function(){
     $('.operands').on('click', 'button', function(){
         var val = $(this).val();
 
-        input_digit(val);
+        inputDigit(val);
     });
 
     $('.ope').on('click', 'button', function(){
        operator = $(this).val();
 
-        increment_array(operator);
+        incrementArray(operator);
     });
 });
 
-function doMath(){
-    var temp_array = num_array.join(' ');
-    result = eval(temp_array);
-    $('#output').text(result);
-    num_array = [''];
-    i = 0;
-    num_array[i] += result;
-}
+//function doMath(){
+//    var temp_array = num_array.join(' ');
+//    result = eval(temp_array);
+//    $('#output').text(result);
+//    num_array = [''];
+//    i = 0;
+//    num_array[i] += result;
+//}
 
 function clearLast() {
     var temp = num_array.join(' ');
@@ -55,39 +60,65 @@ function clearEverything() {
     $('#output').text(0);
     num_array = [''];
     i = 0;
+    result = null;
     operator = null;
+    num1 = null;
+    num2 = null;
+    op = null;
 }
 
 function decimalPoint() {
     num_array[i] += '.';
-    refresh_display();
+    refreshDisplay();
 }
 
 
-//function doMath (num1, num2, op) {
-//    num1 = parseInt(number1);
-//    num2 = parseInt(number2);
-//    op = operator;
-//    var result;
-//    console.log(num1, op, num2);
-//
-//    switch (op) {
-//        case '+':
-//            result = (num1 + num2);
-//            break;
-//        case '-':
-//            result = (num1 - num2);
-//            break;
-//        case '/':
-//            if (num2 === 0) {
-//                result = 'error: can\'t divide by 0! You are messing with dark forces you can\'t even begin to comprehend!';
-//            }
-//            else {
-//                result = (num1 / num2);
-//            }
-//            break;
-//        case 'x':
-//            result = (num1 * num2);
-//            break;
-//    }
-//}
+function doMaths () {
+    for (var i = 0; i < num_array.length; i++) {
+        if (isNaN(num_array[i])) {
+            if (op == null) {
+                op = num_array[i];
+                num1 = parseFloat(num_array[i -1]);
+                num2 = parseFloat(num_array[i +1]);
+
+                result = calculate(num1, num2, op);
+
+                num_array[i -1] = result;
+                num_array.splice(i,2);
+
+                refreshDisplay();
+
+                result = null;
+                operator = null;
+                num1 = null;
+                num2 = null;
+                op = null;
+                i = 0;
+            }
+        }
+    }
+}
+
+function calculate(num1, num2, op) {
+    switch (op) {
+        case '+':
+            result = (num1 + num2);
+            break;
+        case '-':
+            result = (num1 - num2);
+            break;
+        case '/':
+            if (num2 === 0) {
+                result = 'error: can\'t divide by 0! You are messing with dark forces you can\'t even begin to comprehend!';
+            }
+            else {
+                result = (num1 / num2);
+            }
+            break;
+        case '*':
+            result = (num1 * num2);
+            break;
+        //default:
+    }
+    return result;
+}
