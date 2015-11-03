688a7f99185e8eb4982a1ec8691dcb1fb90838eb
Kinarki <nickfriend81@gmail.com>
2015-11-02 17:46:09 -0800

    worked on functionality for 1.0, numbers display, math technically works.

--- script.js
+++ script.js
@@ -1,27 +1,89 @@
-var my_calculator = new calculator(calcFunction);
+//var my_calculator = new Calculator(calcFunction);
+var num_array = [''];
+var i = 0;
+var operator = null;
+var result;
 
+function input_digit(number) {
+    num_array[i] += number;
+    $('#output').text(num_array.join(' '));
+}
 
-function calcFunction(type, value, item){
-    switch(value){
-        case undefined:
-            $('#output').html('');
-            break;
-        default:
-            $('#output').html(value);
-            break;
-    }
+function increment_array(oper) {
+    i++;
+    num_array.push(oper);
+    $('#output').text(num_array.join(' '));
+    i++;
+    num_array.push('');
 }
 
 $(document).ready(function(){
-    $('button').on('click', function(){
+    $('.operands').on('click', 'button', function(){
         var val = $(this).val();
-        switch (val) {
-            case 'CE':
-                my_calculator.allClear();
-                break;
-            default:
-                my_calculator.addItem($(this).val());
-                break;
-        }
+
+        input_digit(val);
+    });
+
+    $('.ope').on('click', 'button', function(){
+       operator = $(this).val();
+
+        increment_array(operator);
     });
 });
+
+function doMath(){
+    var temp_array = num_array.join(' ');
+    result = eval(temp_array);
+    $('#output').text(result);
+    num_array = [''];
+    i = 0;
+    num_array[i] += result;
+}
+
+function clearLast() {
+    var temp = num_array.join(' ');
+    var temp = temp.substring(0, temp.length-1);
+    num_array = temp.split(' ');
+    i = num_array.length-1;
+    $('#output').text(temp);
+}
+
+function clearEverything() {
+    $('#output').text(0);
+    num_array = [''];
+    i = 0;
+    operator = null;
+}
+
+function decimalPoint() {
+    $('#output').text(num_array[i] += '.');
+}
+
+
+//function doMath (num1, num2, op) {
+//    num1 = parseInt(number1);
+//    num2 = parseInt(number2);
+//    op = operator;
+//    var result;
+//    console.log(num1, op, num2);
+//
+//    switch (op) {
+//        case '+':
+//            result = (num1 + num2);
+//            break;
+//        case '-':
+//            result = (num1 - num2);
+//            break;
+//        case '/':
+//            if (num2 === 0) {
+//                result = 'error: can\'t divide by 0! You are messing with dark forces you can\'t even begin to comprehend!';
+//            }
+//            else {
+//                result = (num1 / num2);
+//            }
+//            break;
+//        case 'x':
+//            result = (num1 * num2);
+//            break;
+//    }
+//}
